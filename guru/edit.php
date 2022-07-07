e61c43a58d2fa00f27025a44bc28f4c733a67450
sucioktvni <oktviani064@gmail.com>
2022-07-07 14:16:46 +0700

    mengubah data guru

--- guru/edit.php
+++ guru/edit.php
@@ -41,7 +41,7 @@
         <div class="container">
         <div class="card">
             <div class="card-header">
-                Edit Guru
+                Edit Data Guru
             </div>
             <div class="card-body">
             <?php
